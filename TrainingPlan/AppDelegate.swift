ffb019d347d0620b15ed895be5c5494c836d2147
numpadCrusader <103027096+numpadCrusader@users.noreply.github.com>
2025-07-30 20:21:35 +0500

    added dependencies

--- TrainingPlan/AppDelegate.swift
+++ TrainingPlan/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
